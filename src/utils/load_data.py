e4b180f7c03a60d55eef30ab170d15e7657dc70d
arasaniu <aras.aniulis@gmail.com>
2025-01-11 10:15:52 +0100

    Co-authored-by: Serban Tonie <Serbbi@users.noreply.github.com> Co-authored-by: Alex <Macrou@users.noreply.github.com> Co-authored-by: akr115 <akr115@users.noreply.github.com>

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,3 +1,13 @@
+import numpy as np
 
-def load_data():
-    print("I should load the data but all I currently do is say Hello world!")
+def load_data(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = file.readlines()
+            data_array = [float(line.strip()) for line in data]
+            data_array = np.array(data_array)
+            data_array = (data_array - np.mean(data_array)) / np.std(data_array)
+            return data_array
+    except FileNotFoundError:
+        print("File not found. Please check the path and try again.")
+        return None
