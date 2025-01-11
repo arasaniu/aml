4fe1d6b962113942f68c55e7eb1f2ef6cfbeabc3
arasaniu <aras.aniulis@gmail.com>
2025-01-11 12:31:54 +0100

    update high pass

--- src/plots/plot_signal.py
+++ src/plots/plot_signal.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
 def plot_signal(signal, name, time_steps=2000):
+    plt.clf()
     plt.figure(figsize=(20,5))
     plt.plot(signal[:time_steps], label=name)
     plt.title(name)
     plt.show()
+    plt.clf()
+    
