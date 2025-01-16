92fe4d17b9141c3350a7f81d17c2f883e261ce12
arasaniu <aras.aniulis@gmail.com>
2025-01-16 13:29:33 +0100

    better linear model

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Thorax data serves as input, abdomen data as target
@@ -5,3 +6,7 @@ def train_linear_model(thorax_data, abdomen_data):
     model = LinearRegression()
     model.fit(thorax_data, abdomen_data)
     return model
+
+def create_lagged_features(signal, lags=3):
+    lagged_features = np.hstack([np.roll(signal, shift=i) for i in range(lags)])
+    return lagged_features
